b5043ae441b8d4de0a00bc3e1d989e7c19c85b4d
Ivana Yovcheva <iyovcheva@vmware.com>
2018-10-04 11:50:28 +0300

    Add index.html

--- radio/handler.go
+++ radio/handler.go
@@ -2,9 +2,21 @@ package function
 
 import (
 	"fmt"
+	"log"
+	"os"
 )
 
 // Handle a serverless request
 func Handle(req []byte) string {
-	return fmt.Sprintf("Welcome on RADIO, %s! We're waiting you on the Expo Booth!", string(req))
+	file, err := os.Open("./function/index.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	data := make([]byte, 100)
+	count, err := file.Read(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return fmt.Sprintf("%q", data[:count])
+	// return fmt.Sprintf("Welcome on RADIO, %s!", string(req))
 }
